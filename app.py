4b5fc91630155e2276c2d8203397ba96b9e9038f
AmirGit <aeonthings@gmail.com>
2022-09-20 17:33:26 +0200

    11

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ if authentication_status:
             sq2 = Image.open(r'SQ2.png')
 
             with col0:
-                st.image(sq1, width=1000)
+                st.image(sq1, width=950)
 
             with col00:
                 st.image(sq2)
