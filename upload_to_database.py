d7a5e5a7385596634ce18adf0abe9550fcff84be
AmirGit <aeonthings@gmail.com>
2022-09-20 13:53:08 +0200

    .

--- upload_to_database.py
+++ upload_to_database.py
@@ -1,9 +1,9 @@
 import streamlit_authenticator as stauth
 import database as db
 
-usernames = ["AL", "SG", "JF", "siix", "Donovan", "jb", "Dje", "DVortex", "pap", "starseedmatrix", "Blake_R", "JwJ", "Bones"]
+usernames = ["AL", "SG", "JF", "siix", "Donovan", "jb", "Dje", "DVortex", "pap", "starseedmatrix", "Blake_R", "JwJ", "Bones_123"]
 names = ["Amir Lehmam", "Sachith Galbokka", "Jim Fredrickson", "Justin Cooper", "Douwe Jan", "Jonah the Great", "Dje Valens", "DVortex", "Pap", "Alexandra", "Blake R", "Joe just Joe", "Bones"]
-passwords = ["azerty94", "azerty75", "azerty99", "siix6sixty6", "numbersdontlie69", "Suns#ine882022", "Bassam06", "DVortex", "azerty94", "Starseedmatrix11", "Gann1908", "Vesica-Piscis28", "Bones123"]
+passwords = ["azerty94", "azerty75", "azerty99", "siix6sixty6", "numbersdontlie69", "Suns#ine882022", "Bassam06", "DVortex", "azerty94", "Starseedmatrix11", "Gann1908", "Vesica-Piscis28", "Bones_12354"]
 hashed_passwords = stauth.Hasher(passwords).generate()
 
 for (username, name, hash_password) in zip(usernames, names, hashed_passwords):
