2ebef74c5fd887b10a5bdf27a0c0427060f8e426
AmirGit <aeonthings@gmail.com>
2022-09-20 17:23:08 +0200

    00

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ if authentication_status:
             with col00:
                 st.image(sq2)
                 st.markdown("Here is an aerial view of the **Khufru pyramid in Egypt**, some claim that the ancient builders bequeathed these pyramids to us as an *astro-calculator*, which **W.D Gann** calls the ***Square of 9***. It has been discovered that each **45°** row of this pyramid (**8 sides**) has a **small inclination**, was this intentional to reveal the importance of the **45° degree**? Or as they call it, an architectural coincidence...")
-                original_title = '**<p style="font-family:sans-serif; color:Purple; font-size: 20px;">**Heliocentric**</p>**'
+                original_title = '**<p style="font-family:sans-serif; color:DarkViolet; font-size: 20px;">**Heliocentric**</p>**'
                 st.markdown(original_title, unsafe_allow_html=True)
                 for col in helio.columns[1:]:
                     helio = helio.astype({col: int})
