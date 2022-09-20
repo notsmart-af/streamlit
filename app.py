4b4c944cb78d310f3dfa068ce3793e8f6de1efa8
AmirGit <aeonthings@gmail.com>
2022-09-20 19:17:28 +0200

    1

--- app.py
+++ app.py
@@ -125,8 +125,8 @@ if authentication_status:
                     st.dataframe(hm)
             with col00:
                     st.markdown("**Transit Aspects**")
-                    for col in mn1.columns[1:]:
-                        mn1 = mn1.astype({col: int})
+                    #for col in mn1.columns[1:]:
+                    #    mn1 = mn1.astype({col: int})
                     st.dataframe(mn1.style.background_gradient(cmap='Blues'))
                     st.markdown("**Natal Aspects**")
                     st.dataframe(mn2.style.background_gradient(cmap='Blues'))
