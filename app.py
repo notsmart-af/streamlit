ac52f3ab37208a8c4469b7df0d6b43540288d847
AmirGit <aeonthings@gmail.com>
2022-09-21 07:38:58 +0200

    1

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ if authentication_status:
         figui = px.bar(m, x="Date", y="EP", hover_data=['Date', 'EP'], color='EP', color_continuous_scale=px.colors.sequential.Cividis,
              height=618).update_layout(xaxis={"rangeslider":{"visible":True}})
         st.plotly_chart(figui, use_container_width=True)
-        st.markdown("**Energy Points (EP) Hits on BTC chart last 2 years**")  
+        st.title('**Energy Points Hits on BTC @ last 2 years**')
         df = btcusd_d.copy()
 
         fig = go.Figure(data=[go.Candlestick(x=df['Date'],
