0f3834846b85a5980426312429208faf50d48c03
AmirGit <aeonthings@gmail.com>
2022-09-21 07:03:14 +0200

    1

--- app.py
+++ app.py
@@ -257,16 +257,24 @@ if authentication_status:
                         high=df['High'],
                         low=df['Low'],
                         close=df['Close'])])
-        fig.add_vline(x=1, x0="2021-11-10", x1="2021-11-11", line_width=1, line_color="blue")
+        fig.add_vline(x=1, x0="2021-11-10", x1="2021-11-11", line_width=3, line_color="black")
         fig.add_vline(x=1, x0="2020-12-05", x1="2020-12-05", line_width=1, line_color="blue")
         fig.add_vline(x=1, x0="2020-12-18", x1="2020-12-18", line_width=1, line_color="blue")
-        fig.add_vline(x=1, x0="2021-01-08", x1="2021-01-08", line_width=1, line_color="blue")
+        fig.add_vline(x=1, x0="2021-01-08", x1="2021-01-08", line_width=1, line_color="red")
         fig.add_vline(x=1, x0="2021-01-22", x1="2021-01-22", line_width=1, line_color="blue")        
-        fig.add_vline(x=1, x0="2021-02-17", x1="2021-02-17", line_width=1, line_color="blue")
+        fig.add_vline(x=1, x0="2021-02-17", x1="2021-02-17", line_width=1, line_color="red")
         fig.add_vline(x=1, x0="2021-02-26", x1="2021-02-26", line_width=1, line_color="blue")
-        fig.add_vline(x=1, x0="2021-03-14", x1="2021-03-14", line_width=1, line_color="blue")
+        fig.add_vline(x=1, x0="2021-03-14", x1="2021-03-14", line_width=1, line_color="red")
         fig.add_vline(x=1, x0="2021-03-24", x1="2021-03-24", line_width=1, line_color="blue")
-        fig.add_vline(x=1, x0="2021-04-15", x1="2021-04-15", line_width=1, line_color="blue")
+        fig.add_vline(x=1, x0="2021-04-15", x1="2021-04-15", line_width=3, line_color="black")
+        fig.add_vline(x=1, x0="2021-04-24", x1="2021-04-24", line_width=1, line_color="blue")
+        fig.add_vline(x=1, x0="2021-05-05", x1="2021-05-05", line_width=1, line_color="red")
+        fig.add_vline(x=1, x0="2021-05-18", x1="2021-05-18", line_width=1, line_color="blue")
+        fig.add_vline(x=1, x0="2021-05-26", x1="2021-05-26", line_width=1, line_color="red")
+        fig.add_vline(x=1, x0="2021-06-10", x1="2021-06-10", line_width=1, line_color="blue")
+        fig.add_vline(x=1, x0="2021-06-19", x1="2021-06-19", line_width=1, line_color="red")
+        fig.add_vline(x=1, x0="2021-06-23", x1="2021-06-23", line_width=3, line_color="black")
+        fig.add_vline(x=1, x0="2021-07-06", x1="2021-07-06", line_width=1, line_color="red")        
 
         fig.update_layout(
         autosize=False,
