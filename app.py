863a6b9dfb9ba7f1511ad659e337ca0886b11183
AmirGit <aeonthings@gmail.com>
2022-09-25 16:08:54 +0200

    11

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ if authentication_status:
                     st.write('')
                 with col2:
                     sp = Image.open(r'sq9.png')
-                    st.image(sp)
+                    st.image(sp, caption="Square of 9", use_column_width='always')
                 with col3:
                     st.write('')
 
