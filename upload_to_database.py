48845386292a45a44fdafcba45c1b42b1e12f876
AmirGit <aeonthings@gmail.com>
2022-09-25 17:55:00 +0200

    11

--- upload_to_database.py
+++ upload_to_database.py
@@ -1,9 +1,9 @@
 import streamlit_authenticator as stauth
 import database as db
 
-usernames = ["Am", "SG", "JF", "siix", "Donovan", "jb", "Dje", "DVortex", "pap", "starseedmatrix", "Blake_R", "JwJ", "Bones_123", "mystery", "Penumbra","Jonak","Ershad", "Rami", "Moe", "Guest"]
-names = ["Amir Lehmam", "Sachith Galbokka", "Jim Fredrickson", "Justin Cooper", "Douwe Jan", "Jonah the Great", "Dje Valens", "DVortex", "Pap", "Alexandra", "Blake R", "Joe just Joe", "Bones", "Mystery", "Penumbra", "Jonak", "Ershad", "Rami", "Moe", "Guest"]
-passwords = ["amirdu94", "azerty75", "azerty99", "siix6sixty6", "numbersdontlie69", "Suns#ine882022", "Bassam06", "DVortex", "azerty94", "Starseedmatrix11", "Gann1908", "Vesica-Piscis28", "Bones_12354", "test123", "Penum123", "Jonak123", "Fish123", "Ram123", "Moe123", "Guest123"]
+usernames = ["Am", "SG", "JF", "siix", "Donovan", "jb", "Dje", "DVortex", "pap", "starseedmatrix", "Blake_R", "JwJ", "Bones_123", "mystery", "Penumbra","Jonak","Ershad", "Rami", "Moe", "Guest", "Test"]
+names = ["Amir Lehmam", "Sachith Galbokka", "Jim Fredrickson", "Justin Cooper", "Douwe Jan", "Jonah the Great", "Dje Valens", "DVortex", "Pap", "Alexandra", "Blake R", "Joe just Joe", "Bones", "Mystery", "Penumbra", "Jonak", "Ershad", "Rami", "Moe", "Guest", "Test"]
+passwords = ["amirdu94", "azerty75", "azerty99", "siix6sixty6", "numbersdontlie69", "Suns#ine882022", "Bassam06", "DVortex", "azerty94", "Starseedmatrix11", "Gann1908", "Vesica-Piscis28", "Bones_12354", "test123", "Penum123", "Jonak123", "Fish123", "Ram123", "Moe123", "Guest123", "Test123"]
 hashed_passwords = stauth.Hasher(passwords).generate()
 
 for (username, name, hash_password) in zip(usernames, names, hashed_passwords):
