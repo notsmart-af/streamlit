94ec366c4e7f7e5faf6d8ace4e9fdc3021260bb0
AmirGit <aeonthings@gmail.com>
2022-09-27 14:27:37 +0200

    11

--- app.py
+++ app.py
@@ -755,9 +755,8 @@ if authentication_status:
                     highlight = ['Positive', 'Strongly Positive', "Weakly Positive"]
                     st.dataframe(gdeg.style.bar(subset="compound", color=['red', 'green'], align='zero'))
                 with col3:
-                    st.markdown('**Scrapped Tweets Word Cloud (#BITCOIN)**')
                     sss = Image.open(r'wc1.png')
-                    st.image(sss)
+                    st.image(sss, width=850 caption='WordCloud of our scrapped Tweets (#Bitcoin)')
 
                 fig = px.line(gdeg, x="Date", y="Sentiment", color='Sentiment', title="Sentiments grouped by Dates", width=1618, height=500, symbol="Sentiment")
                 fig.update_layout(xaxis=dict(showgrid=False),
